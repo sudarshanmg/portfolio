ebbbe2569692fe6fd9c56c7a9ffbb8443a09125d
sudarshan <sudarshanmallibhat@gmail.com>
2023-09-12 22:00:27 +0530

    delete api dir

--- app/api/fetch-notes/route.ts
+++
@@ -1,7 +0,0 @@
-import { sql } from "@vercel/postgres";
-import { NextResponse } from "next/server";
-
-export async function GET(request: Request) {
-  const notes = await sql`SELECT * FROM notes;`;
-  return NextResponse.json({ notes }, { status: 200 });
-}
