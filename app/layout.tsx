3bdd88bcabbcb875a0001be5f39946e62320cc7c
sudarshan <sudarshanmallibhat@gmail.com>
2024-07-22 12:27:43 +0530

    background and whoami section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,38 +1,37 @@
-import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
-import Navbar from "@/components/Navbar";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import ModeToggle from "@/components/ModeToggle";
+import './globals.css';
+import { ThemeProvider } from '@/components/theme-provider';
+import Navbar from '@/components/Navbar';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import Image from 'next/image';
+import Sudobg from '@/public/images/bg2.jpg';
 
-const font = Inter({ subsets: ["latin"] });
+const font = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-	title: "Sudarshan",
-	description: "Software Developer",
+  title: 'Sudarshan',
+  description: 'Software Developer',
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html
-			lang="en"
-			suppressHydrationWarning
-		>
-			<body className={font.className}>
-				<ThemeProvider
-					attribute="class"
-					defaultTheme="dark"
-					enableSystem
-				>
-					<Navbar />
-					{children}
-					<ModeToggle />
-				</ThemeProvider>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${font.className} relative h-fit`}>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+          <Navbar />
+          <Image
+            fill={true}
+            src={Sudobg}
+            alt="Backdrop"
+            className="z-[-1] absolute bg-repeat opacity-20 overflow-x-hidden object-cover"
+          />
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,39 @@
-import Image from "next/image";
-import Star from "@/public/images/star.svg";
+import Image from 'next/image';
+import Star from '@/public/images/star.svg';
 
-import Terminal from "@/components/Terminal";
+import Terminal from '@/components/Terminal';
 
 export default function Home() {
-	return (
-		<main>
-			<div className="text-center my-16 mx-4 max-h-fit">
-				<header className="font-playfair">
-					<div className="hidden lg:block relative">
-						<Image
-							src={Star}
-							width={40}
-							height={40}
-							alt="star bro"
-							className="right-60 absolute"
-						/>
-					</div>
-					<h1 className="lg:text-8xl sm:text-4xl md:text-6xl text-4xl font-acorn">
-						<span>{`Hi. I'm Sudarshan.`}</span> <br />
-						<span>A Developer.</span>
-					</h1>
-					<div className="hidden lg:block relative">
-						<Image
-							src={Star}
-							width={40}
-							height={40}
-							alt="star bro"
-							className="left-60 absolute bottom-4"
-						/>
-					</div>
-					<h2 className="md:text-md text-2xl m-4 font-mono text-neutral-500">{`and Batman by night.`}</h2>
-				</header>
-			</div>
-			<Terminal />
-		</main>
-	);
+  return (
+    <main className="flex flex-col justify-stretch">
+      <div className="text-center my-16 mx-4 max-h-fit">
+        <header className="font-playfair">
+          <div className="hidden lg:block relative">
+            <Image
+              src={Star}
+              width={40}
+              height={40}
+              alt="star bro"
+              className="right-60 absolute"
+            />
+          </div>
+          <h1 className="lg:text-8xl sm:text-5xl md:text-7xl text-6xl font-acorn">
+            <div>{`Hi. I'm Sudarshan.`}</div>
+            <div className=" font-extralight">El creator.</div>
+          </h1>
+          <div className="hidden lg:block relative">
+            <Image
+              src={Star}
+              width={40}
+              height={40}
+              alt="star bro"
+              className="left-60 absolute bottom-4"
+            />
+          </div>
+          <h2 className="md:text-md text-2xl m-4 font-mono font-semibold text-neutral-500">{`I design and build things.`}</h2>
+        </header>
+      </div>
+      <Terminal />
+    </main>
+  );
 }
